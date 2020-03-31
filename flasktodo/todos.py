61415394bf30a17c2e5800012bed4f0755ae91f0
Eric Moyer <ejmoyer3@comcast.net>
2020-03-31 13:02:38 -0400

    Fixed tests, made get todos cleaner.

--- flasktodo/todos.py
+++ flasktodo/todos.py
@@ -8,16 +8,16 @@ bp = Blueprint("todos", __name__)
 @bp.route("/", methods=("GET", "POST"))
 def index():
     """View for home page which shows list of to-do items."""
+    conn = db.get_db()
+    cur = conn.cursor()
+
     if request.method == 'POST':
         newtask = request.form['newtask']
 
-        cur = db.get_db().cursor()
         cur.execute("INSERT INTO todos (description, completed, created_at) VALUES (%s, FALSE, NOW())",
                     (newtask,))
-        db.get_db().commit()
-        cur.close()
+        conn.commit()
 
-    cur = db.get_db().cursor()
     cur.execute('SELECT * FROM todos')
     todos = cur.fetchall()
     cur.close()
